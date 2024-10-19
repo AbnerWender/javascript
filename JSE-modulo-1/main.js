4d9ccd69c088b4a3fd554e2f74ee523781826f33
AbnerWender <abnderwender@gmail.com>
2024-10-19 12:00:44 -0400

    criando a separação do conteudo do curso

---
+++ JSE-modulo-1/main.js
@@ -0,0 +1,24 @@
+console.log("Eaaeee, Abnerrrr!");
+
+
+// questionário da seção
+
+// Pergunta 1: Use console.log para enviar seu nome completo para o console.
+console.log("Abner Wender");
+
+// Pergunta 2: Informe seu ano de nascimento.
+console.log("2006");
+
+// Pergunta 3: Tente novamente digitar seu ano de nascimento, desta vez passando a data sem as aspas.
+console.log(2006);
+
+// Pergunta 4: Podemos passar vários argumentos para console.log separados por vírgulas, por exemplo: console.log("abc", "def", "ghi");
+console.log("Abner", "Wender", "2006");
+
+// Pergunta 5: Envie as mesmas informações (nome, sobrenome, ano) para o console, não lado a lado, mas em linhas consecutivas.
+console.log("Abner");
+console.log("Wender");
+console.log("2006");
+
+// Pergunta 6: Uma string pode ser concatenada usando o + sinal, por exemplo "abc" + "def" será tratado como "abcdef". Tente escrever seu nome, sobrenome e ano de nascimento em uma linha novamente, desta vez não separados por vírgulas, mas por + sinais.
+console.log("Abner" + " Wender" + " 2006");
